bed1351b4c9046825a4b7ec4b0a35bd023840929
will-hsiao <xiao@ms4.url.com.tw>
2014-09-02 22:51:42 +0800

    on going

--- rps_oo.rb
+++ rps_oo.rb
@@ -1,4 +1,9 @@
 #Lesson 2 assignment
+#The player 1 choose from a set of [Rock, Paper, Scissors], then the player 2 choose from the set of [Rock, Paper, Scissors]
+#Than compare the player1's choice with the player2's choice to decide which one wins or a tie.
+#Class: Set of Rock, paper, scissors
+#Class: Player
+
 #-----------Rock Paper Scissor
 class RockPaperScissors
 	attr_accessor :choice, :gesture
@@ -13,21 +18,6 @@ class RockPaperScissors
 	def initialize
 		@choice = nil
 		@gesture = nil
-	end
-
-
-	def choose_by_input
-		begin
-			puts "Choose one: R/P/S"
-			@gesture=gets.chomp
-		end while @gesture != 'r' && @gesture != 'p' && @gesture != 's'
-		@choice = @@gesture_to_i[@gesture]
-
-	end
-
-	def choose_by_rand
-		@choice = rand(3)
-		@gesture =@@choice_to_g[@choice]
 	end
 
 	def arbitrate(ch2)
@@ -46,6 +36,35 @@ class RockPaperScissors
 
 end #end class
 
+class Player
+	attr_accessor :name, :choice
+	def initailize(name)
+		@name=name
+	end
+	def choose_by_input
+		begin
+			puts "Choose one: R/P/S"
+			@choice=gets.chomp
+		end while @choice != 'r' && @choice != 'p' && @choice != 's'
+		@choice = @@gesture_to_i[@gesture]
+
+	end
+
+	def choose_by_rand
+		@choice = rand(3)
+		@gesture =@@choice_to_g[@choice]
+	end
+
+end #end class Player
+
+class Game
+
+	def initialize(name1, name2)
+		player1 = Player.new(name1)
+		player2 = Player.new(name2)
+	end
+
+
 
 #----------Sceniaro------------------
 #get user's gesture
@@ -54,22 +73,30 @@ end #end class
 #get computer's gesture
 #compare user's vs. computer's
 #print result
+	def run
 
-begin
-	puts "---------Play Rock Paper Scissors!----------------"
+		begin
+			puts "---------Play Rock Paper Scissors!----------------"
 
-	player1 = RockPaperScissors.new
-	player2 = RockPaperScissors.new
+			player1 = RockPaperScissors.new
+			player2 = RockPaperScissors.new
 
-	player1.choose_by_input
-	player2.choose_by_rand
+			player1.choose_by_input
+			player2.choose_by_rand
 
-	puts "You picked #{player1.gesture}, and Computer picked #{player2.gesture}"
+			puts "You picked #{player1.gesture}, and Computer picked #{player2.gesture}"
 
-	player1.arbitrate(player2.choice)
+			player1.arbitrate(player2.choice)
 
-	puts "Do you want to play again? (Y/N)"
-	play=gets.chomp
-	play = (play == 'Y' || play =='y') ? TRUE: FALSE
+			puts "Do you want to play again? (Y/N)"
+			play=gets.chomp
+			play = (play == 'Y' || play =='y') ? TRUE: FALSE
 
-end while play==TRUE
+		end while play==TRUE
+	end
+
+end #end Game Class
+
+Game.new.run
+
+
